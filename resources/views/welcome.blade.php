59ab76327575abc138d6e46fe960fb8bf8ddd580
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2023-05-13 08:34:54 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('styles')
+@endsection
+@section('content')
+<header style="height: 90vh">
+  <div class="container h-100">
+    <div class="row h-100 align-items-center">
+      <div class="col-12 text-center">
+        <h1 class="fw-light">Universidad de Dagupan</h1>
+        <p class="lead">School Automate</p>
+      </div>
+    </div>
+  </div>
+</header>
+@endsection
